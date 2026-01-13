cdc0d741a70aa6339e424733dd49ada92d4af093
magwar <williammagua@gmail.com>
2026-01-14 00:07:43 +0300

    Refactor Button component to use named export and update import in App component

--- hello-world/src/Button.jsx
+++ hello-world/src/Button.jsx
@@ -1,9 +1,25 @@
-function Button() {
-  return <button>Click Me</button>;
+import React from 'react'
+
+export const Button = () => {
+  return (
+    <button>Button With Arrow Function</button>
+  )
 }
-export default Button;
+
+
+// function Button() {
+//   return <button>Click Me</button>;
+// }
+// export default Button;
 
 // Arrow function syntax
 // const Button = () => {
 // <button>Click Me</button>
 // }
+
+// default exports vs named exports
+// export default Button
+// import Button from './Button'
+
+// export const Button = () => {}
+//import { Button } from './Button'
--- hello-world/src/Welcome.jsx
+++ hello-world/src/Welcome.jsx
@@ -7,5 +7,18 @@ export default Welcome
 
 // Arrow function syntax
 // const Welcome = () => {
-    //   <div>Welcome William</div>
+    //   <div>Welcome, William</div>
 // }
+
+// default exports vs named exports
+// with export default Welcome one must import like so:
+// import Welcome from './Welcome'
+// The component can be renamed during import like so:
+// import AmazingWelcome from './Welcome' and then used as <AmazingWelcome /> in JSX
+// without affecting the export name in Welcome.jsx and causing an error
+
+// export const Welcome = () => {} ia a named export and must be imported like so:
+//import { Welcome } from './Welcome'
+//The component must be used with the same name as exported and can not be changed 
+// during import
+// This prevents naming confusion when importing multiple components
